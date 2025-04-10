154ff22447fae242e7f13cf16580dfbcc89b9454
KYuSamsonov <KYuSamsonov@tum.beeline.ru>
2025-04-10 11:35:26 +0300

    SFDM-2488 Разработать метод POST для записи инфрастуктуры под приложение

--- src/main/java/ru/beeline/fdmproducts/repository/ProductRepository.java
+++ src/main/java/ru/beeline/fdmproducts/repository/ProductRepository.java
@@ -17,6 +17,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT p FROM Product p JOIN p.techProducts tp WHERE tp.deletedDate IS NULL")
     List<Product> findAllWithTechProductNotDeleted();
 
-    @Query("SELECT p.alias FROM Product p JOIN p.techProducts tp WHERE tp.deletedDate IS NULL")
-    List<String> findAllAliasesWithTechProductNotDeleted();
+    @Query("SELECT p.alias FROM Product p")
+    List<String> findAllAliases();
 }

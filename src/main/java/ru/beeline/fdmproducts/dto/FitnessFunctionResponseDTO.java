ce1c9ddcdc1327392bb70dfa90172bf6cd23ba1e
salikhanov <SAlikhanov@oms.beeline.ru>
2025-11-08 19:30:34 +0600

    SFDM-3022 Доработать метод полчения расчитанных фитнес-функций по продукту

--- src/main/java/ru/beeline/fdmproducts/dto/FitnessFunctionResponseDTO.java
+++ src/main/java/ru/beeline/fdmproducts/dto/FitnessFunctionResponseDTO.java
@@ -2,16 +2,23 @@ package ru.beeline.fdmproducts.dto;
 
 import lombok.*;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @Builder
 public class FitnessFunctionResponseDTO {
+
     private Integer id;
     private String code;
     private String description;
     private Boolean isCheck;
     private String resultDetails;
     private String status;
+    private String assessmentDescription;
+    private String docLink;
+    private List<AssessmentObjectDTO> details;
+    private List<String> tableStruct;
 }

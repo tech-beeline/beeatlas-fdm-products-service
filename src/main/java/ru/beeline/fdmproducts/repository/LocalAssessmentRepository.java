59ec9805f6cc6583d9cf913e40208edf823a397b
Konstantin <KYuSamsonov@tum.beeline.ru>
2026-06-17 14:25:25 +0300

    Revert "SFDM-3819"
    This reverts commit bc0a47679abb2f55c4da3047dbaa8e432d0adf3b.

--- src/main/java/ru/beeline/fdmproducts/repository/LocalAssessmentRepository.java
+++ src/main/java/ru/beeline/fdmproducts/repository/LocalAssessmentRepository.java
@@ -11,7 +11,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.beeline.fdmproducts.domain.LocalAssessment;
 import ru.beeline.fdmproducts.domain.Product;
-import ru.beeline.fdmproducts.dto.LatestAssessmentCheckDTO;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,18 +35,20 @@ public interface LocalAssessmentRepository extends JpaRepository<LocalAssessment
     @Query("DELETE FROM LocalAssessment la WHERE la.product.id = :productId")
     void deleteByProductId(@Param("productId") Integer productId);
 
-    @Query("SELECT la.id, la.product.id FROM LocalAssessment la " +
-            "WHERE la.product.id IN :productIds " +
-            "ORDER BY la.product.id ASC, la.createdTime DESC")
-    List<Object[]> findAssessmentIdsOrderedByProductAndTime(@Param("productIds") List<Integer> productIds);
-
-    @Query("SELECT NEW ru.beeline.fdmproducts.dto.LatestAssessmentCheckDTO(" +
-            "    lac.assessment.product.id, " +
-            "    lac.id, " +
-            "    lac.isCheck, " +
-            "    lac.fitnessFunction.id" +
-            ") " +
-            "FROM LocalAssessmentCheck lac " +
-            "WHERE lac.assessment.id IN :assessmentIds")
-    List<LatestAssessmentCheckDTO> findChecksByAssessmentIds(@Param("assessmentIds") List<Integer> assessmentIds);
+    @Query(value = """
+            SELECT
+                la.product_id  AS "productId",
+                lac.id         AS "lacId",
+                lac.is_check   AS "isCheck",
+                lac.lff_id     AS "fitnessFunctionId"
+            FROM product.local_assessment la
+            JOIN product.local_assessment_check lac ON lac.assessment_id = la.id
+            WHERE la.id IN (
+                SELECT DISTINCT ON (product_id) id
+                FROM product.local_assessment
+                WHERE product_id IN (:productIds)
+                ORDER BY product_id, created_time DESC
+            )
+            """, nativeQuery = true)
+    List<LatestAssessmentCheckProjection> findLatestChecksForProducts(@Param("productIds") List<Integer> productIds);
 }
